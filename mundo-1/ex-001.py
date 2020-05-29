18f4607f5c8ed0aa4b59a8df6bdbda70de18e291
guilhermesm <devguilhermemoreira@gmail.com>
2020-05-29 01:04:09 -0300

    Exercício 001 - Olá Mundo

---
+++ mundo-1/ex-001.py
@@ -0,0 +1,11 @@
+# Crie um programa que escreva "Olá, Mundo!" na tela.
+
+print('-' * 100)
+print('{: ^100}'.format('EXERCÍCIO 001 - OLÁ MUNDO'))
+print('-' * 100)
+
+print('Olá, Mundo!')
+
+print('-' * 100)
+
+input('\nPressione ENTER para sair...')
